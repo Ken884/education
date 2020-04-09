e21ee7e620527654c7bcd138bbecd155914e2031
hayashi.kensuke <hayashi.kensuke@dcf.co.jp>
2020-04-09 18:26:10 +0900

    4/9

--- Java/quiz001/Hello.java
+++
@@ -1,8 +0,0 @@
-package quiz001;
-
-public class Hello {
-	public static void main(String[] arg) {
-	System.out.println("Hello World");
-
- }
-}
--- Java/quiz021/quiz021.java
+++
@@ -1,8 +0,0 @@
-package quiz021;
-
-public class quiz021 {
-	public static void main(String[] args) {
-		String[] favoriteFoods = {"pizza", "pasta", "grillia", "formaggio"};
-	}
-
-}
